d70280d1c9132d600e5423e828a7091d900a4c24
Amna2327 <ahmadalisukhera7849@gmail.com>
2025-05-07 19:13:48 +0500

    save it

--- EndSemesterProject.java
+++ EndSemesterProject.java
@@ -1,5 +1,6 @@
 public class EndSemesterProject{
     public static void main(String args[]){
         System.out.println("Lets go");
+        System.out.println("Why is nothing happening");
     }
 }
