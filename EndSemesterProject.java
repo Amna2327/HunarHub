feeb0161f4a0f7dba239b0470ab0ad39fa00ae76
Amna2327 <ahmadalisukhera7849@gmail.com>
2025-05-07 19:14:21 +0500

    start anew

--- EndSemesterProject.java
+++
@@ -1,6 +0,0 @@
-public class EndSemesterProject{
-    public static void main(String args[]){
-        System.out.println("Lets go");
-        System.out.println("Why is nothing happening");
-    }
-}
